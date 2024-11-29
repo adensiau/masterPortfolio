db602a5c2175360d327e83c87456796cff33c850
aden <140476297+adensiau@users.noreply.github.com>
2024-11-29 14:53:07 +1100

    Update EducationComponent.js

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -3,12 +3,12 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import Educations from "../../containers/education/Educations";
-import Certifications from "../../containers/certifications/Certifications";
+// import Certifications from "../../containers/certifications/Certifications";
 import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
 import { competitiveSites } from "../../portfolio";
 // import { certifications } from "../../portfolio";
-import "./EducationComponent.css";
+// import "./EducationComponent.css";
 import { Fade } from "react-reveal";
 
 class Education extends Component {
